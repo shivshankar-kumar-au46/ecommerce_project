9833c5b470b990200bf43fcda58351344eeef9ee
shiv shankar kumar <kumarshiv417@gmail.com>
2024-04-27 10:37:09 +0530

    feat: homepage created

---
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import mainCarouselData from './maincarouseldata';
+
+const items = mainCarouselData.map(item => <img src={item.image} alt=""/>);
+
+const MainCarousel = () => (
+    <AliceCarousel
+        items={items}
+        disableButtonsControls
+        autoPlay
+        autoPlayInterval={2000}
+        infinite
+    />
+);
+
+export default MainCarousel;
