5356330ebb82ac60e0702b1ac287c5d5138e4ef1
shiv shankar kumar <kumarshiv417@gmail.com>
2024-04-27 10:38:47 +0530

    feat: carousel data added

---
+++ src/customer/components/HomeCarousel/maincarouseldata.js
@@ -0,0 +1,21 @@
+const mainCarouselData = [
+    {
+        image:"https://images-eu.ssl-images-amazon.com/images/G/31/img23/Consumables/X-GL/Feb5/PC_Hero_1_3000._CB582457311_.jpg",
+        path:"",
+    },
+    {
+        image:"https://images-eu.ssl-images-amazon.com/images/G/31/img22/march/brands/GW/Under_1499_Tallhero_3000x1200._CB561212093_.jpg",
+        path:"",
+    },
+    {
+        image:"https://images-eu.ssl-images-amazon.com/images/G/31/img22/march/brands/GW/Under_1499_Tallhero_3000x1200._CB561212093_.jpg",
+        path:"",
+    },
+    {
+        image:"https://images-eu.ssl-images-amazon.com/images/G/31/img22/march/brands/GW/Under_1499_Tallhero_3000x1200._CB561212093_.jpg",
+        path:"",
+    }
+
+]
+
+export default mainCarouselData
